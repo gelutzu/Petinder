e26298c0b628fbc0d866b5b536dc1f6dd2230785
ahmedakhtar11 <ahmedakhtar11@yahoo.com>
2018-01-31 01:50:27 -0600

    final

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -11,7 +11,7 @@ $('.counter').each(function() {
 
   {
 
-    duration: 8000,
+    duration: 7000,
     easing:'linear',
     step: function() {
       $this.text(commaSeparateNumber(Math.floor(this.countNum)));
@@ -25,6 +25,32 @@ $('.counter').each(function() {
 
 });  
 
+//Counter2------------------------------------------------------------
+$('.counter2').each(function () {
+    var $this = $(this),
+        countTo = $this.attr('data-count');
+
+    $({ countNum: $this.text() }).animate({
+        countNum: countTo
+    },
+
+        {
+
+            duration: 400000,
+            easing: 'linear',
+            step: function () {
+                $this.text(commaSeparateNumber(Math.floor(this.countNum)));
+            },
+            complete: function () {
+                $this.text(commaSeparateNumber(this.countNum));
+
+            }
+
+        });
+
+});  
+
+//Comma Seprator---------------------------------------------------------
 function commaSeparateNumber(val) {
   while (/(\d+)(\d{3})/.test(val.toString())) {
     val = val.toString().replace(/(\d+)(\d{3})/, '$1' + ',' + '$2');
@@ -32,29 +58,4 @@ function commaSeparateNumber(val) {
   return val;
 }
 
-
-// number of drops created.
-var nbDrop = 900; 
-
-// function to generate a random number range.
-function randRange( minNum, maxNum) {
-  return (Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum);
-}
-
-// function to generate drops
-function createRain() {
-
-  for( i=1;i<nbDrop;i++) {
-  var dropLeft = randRange(0,1400);
-  var dropTop = randRange(-1000,1200);
-
-
-  $('.rain').append('<div class="drop" id="drop'+i+'"></div>');
-  $('#drop'+i).css('left',dropLeft);
-  $('#drop'+i).css('top',dropTop);
-  }
-
-}
-// Make it rain
-createRain();
 
